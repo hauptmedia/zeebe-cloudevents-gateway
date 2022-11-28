5d4d0fb2607540c41dc58c9ffa912e0358acfb9a
Julian Haupt <julian@haupt.media>
2022-11-28 21:51:23 +0100

    add http/2 listener

---
+++ listener.ts
@@ -0,0 +1,44 @@
+import {createSecureServer} from "node:http2";
+import {readFileSync} from "node:fs";
+
+import {CloudEventV1, HTTP} from "cloudevents";
+
+const port = 7777,
+    host = "0.0.0.0";
+
+const server = createSecureServer({
+    cert: readFileSync("certs/localhost-cert.pem"),
+    key: readFileSync("certs/localhost-privkey.pem"),
+    allowHTTP1: true
+});
+
+const cloudEventHandler = (cloudevent: CloudEventV1<any>) => {
+
+    console.log(cloudevent.type);
+
+}
+
+server.on('request', async(req, res) => {
+    const chunks = [];
+
+    for await (const chunk of req) {
+        chunks.push(Buffer.from(chunk));
+    }
+
+    const body = Buffer.concat(chunks).toString("utf-8"),
+        receivedEvent = HTTP.toEvent<object>({ headers: req.headers, body: body });
+
+    if (Array.isArray(receivedEvent))
+        receivedEvent.forEach(cloudEventHandler);
+    else
+        cloudEventHandler(receivedEvent);
+
+    res.end();
+
+});
+
+server.listen({
+    port, host
+}, () => {
+    console.info(`Server is running on https://${host}:${port}`)
+});

5aa4b3bfd8ad1e2d6d3e99c757a56127d61c6c56
Julian Haupt <julian@haupt.media>
2022-12-01 17:13:48 +0100

    add missing commands to registry

--- lib/consumer/GrpcHandlerRegistry.ts
+++ lib/consumer/GrpcHandlerRegistry.ts
@@ -1,8 +1,15 @@
 import {
-    CreateProcessInstanceRequest,
-    DeployResourceRequest,
+    CompleteJobRequest,
+    CreateProcessInstanceRequest, CreateProcessInstanceWithResultRequest,
+    DeployResourceRequest, FailJobRequest,
     GatewayClient,
-    TopologyRequest
+    ModifyProcessInstanceRequest,
+    PublishMessageRequest,
+    ResolveIncidentRequest,
+    SetVariablesRequest,
+    ThrowErrorRequest,
+    TopologyRequest,
+    UpdateJobRetriesRequest
 } from "@hauptmedia/zeebe-gateway-types";
 import {ClientUnaryCall, ServiceError} from "@grpc/grpc-js";
 
@@ -28,13 +35,42 @@ const handler = (grpcServiceFunction: GRPCHandler, grpcRequestFactory: GRPCReque
 }
 
 export const GrpcHandlerRegistry = {
-    'io.zeebe.command.v1.DeployResourceRequest': (zbc: GatewayClient, data: any) =>
-        handler(zbc.deployResource.bind(zbc), DeployResourceRequest, data),
+    'io.zeebe.command.v1.CancelProcessInstanceRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.cancelProcessInstance.bind(zbc), CompleteJobRequest, data),
+
+    'io.zeebe.command.v1.CompleteJobRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.completeJob.bind(zbc), CompleteJobRequest, data),
 
     'io.zeebe.command.v1.CreateProcessInstanceRequest': (zbc: GatewayClient, data: any) =>
         handler(zbc.createProcessInstance.bind(zbc), CreateProcessInstanceRequest, data),
 
-    'io.zeebe.command.v1.TopologyRequest': (zbc: GatewayClient, data: any) =>
-        handler(zbc.topology.bind(zbc), TopologyRequest, data)
+    'io.zeebe.command.v1.CreateProcessInstanceWithResultRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.createProcessInstanceWithResult.bind(zbc), CreateProcessInstanceWithResultRequest, data),
 
+    'io.zeebe.command.v1.DeployResourceRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.deployResource.bind(zbc), DeployResourceRequest, data),
+
+    'io.zeebe.command.v1.FailJobRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.failJob.bind(zbc), FailJobRequest, data),
+
+    'io.zeebe.command.v1.ModifyProcessInstanceRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.modifyProcessInstance.bind(zbc), ModifyProcessInstanceRequest, data),
+
+    'io.zeebe.command.v1.PublishMessageRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.publishMessage.bind(zbc), PublishMessageRequest, data),
+
+    'io.zeebe.command.v1.ResolveIncidentRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.resolveIncident.bind(zbc), ResolveIncidentRequest, data),
+
+    'io.zeebe.command.v1.SetVariablesRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.setVariables.bind(zbc), SetVariablesRequest, data),
+
+    'io.zeebe.command.v1.ThrowErrorRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.throwError.bind(zbc), ThrowErrorRequest, data),
+
+    'io.zeebe.command.v1.TopologyRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.topology.bind(zbc), TopologyRequest, data),
+
+    'io.zeebe.command.v1.UpdateJobRetriesRequest': (zbc: GatewayClient, data: any) =>
+        handler(zbc.updateJobRetries.bind(zbc), UpdateJobRetriesRequest, data)
 }

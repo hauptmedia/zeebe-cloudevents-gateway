1cfa71062ff384513c263b24e8047c612aea3101
Julian Haupt <julian@haupt.media>
2022-12-02 14:18:42 +0100

    made everything configurable via enviroment variables, closes #4

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+ENDPOINT=https://127.0.0.1:8080
+
 echo
 echo "HTTP/1.1 Tests"
-curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @DeployResourceRequest.json https://127.0.0.1:7777
-curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @CreateProcessInstanceRequest.json https://127.0.0.1:7777
-curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @TopologyRequest.json https://127.0.0.1:7777
+curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @DeployResourceRequest.json ${ENDPOINT}
+curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @CreateProcessInstanceRequest.json ${ENDPOINT}
+curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @TopologyRequest.json ${ENDPOINT}
 
 echo
 echo "HTTP/2 Tests"
-curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @DeployResourceRequest.json https://127.0.0.1:7777
-curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @CreateProcessInstanceRequest.json https://127.0.0.1:7777
-curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @TopologyRequest.json https://127.0.0.1:7777
+curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @DeployResourceRequest.json ${ENDPOINT}
+curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @CreateProcessInstanceRequest.json ${ENDPOINT}
+curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @TopologyRequest.json ${ENDPOINT}

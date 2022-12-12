ef69f6de21b3efe298da23831eb3d82329044144
Julian Haupt <julian@haupt.media>
2022-12-12 23:15:23 +0100

    optimize test script

--- tests/run.sh
+++ tests/run.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-ENDPOINT=https://127.0.0.1:8080
-
-echo
+ENDPOINT=https://127.0.0.1:4444
+TESTS="DeployResourceRequest CreateProcessInstanceRequest TopologyRequest ActivateJobsRequest"
 echo "HTTP/1.1 Tests"
-curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @DeployResourceRequest.json ${ENDPOINT}
-curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @CreateProcessInstanceRequest.json ${ENDPOINT}
-curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @TopologyRequest.json ${ENDPOINT}
 
-echo
-echo "HTTP/2 Tests"
-curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @DeployResourceRequest.json ${ENDPOINT}
-curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @CreateProcessInstanceRequest.json ${ENDPOINT}
-curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @TopologyRequest.json ${ENDPOINT}
+for testcase in $TESTS; do
+  echo
+  echo [HTTP/1.1] $testcase
+  echo
+  curl -X POST --http1.1 --insecure -H "content-type: application/cloudevents+json" -d @$testcase.json ${ENDPOINT}
+  echo
+  echo
+  echo [HTTP/2] $testcase
+  echo
+  curl -X POST --http2 --insecure -H "content-type: application/cloudevents+json" -d @$testcase.json ${ENDPOINT}
+  echo
+done

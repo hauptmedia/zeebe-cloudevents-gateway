44635eb1ce6a1a5f546b04b1991940c9b182b68c
Julian Haupt <julian@haupt.media>
2022-12-13 21:52:27 +0100

    handle command rejection

--- src/cloudevents/CloudeventsHandler.ts
+++ src/cloudevents/CloudeventsHandler.ts
@@ -2,6 +2,7 @@ import {CloudEvent, CloudEventV1, HTTP} from "cloudevents";
 import Ajv from "ajv";
 import {ZeebeClient} from "../zeebe/ZeebeClient";
 import {Http2ServerResponse, IncomingHttpHeaders} from "http2";
+import {RejectionType} from "@hauptmedia/zeebe-exporter-types";
 
 const ajv = new Ajv()
 
@@ -25,36 +26,56 @@ export class CloudeventsHandler {
 
     async _handleCloudevent(cloudevent: CloudEventV1<any>): Promise<CloudEventV1<any>> {
         //TODO: validate JSON Schema const data = schemaValidator(schema, cloudevent.data);
+        try {
 
-        const grpcType = cloudevent.type.replace("io.zeebe.command.v1.","");
-        const responseData = await this.zbc.request(grpcType, cloudevent.data);
+            const grpcType = cloudevent.type.replace("io.zeebe.command.v1.", "");
+            const responseData = await this.zbc.request(grpcType, cloudevent.data);
 
-        return new CloudEvent({
-            type: cloudevent.type.replace("Request", "Response"),
-            source: 'source',
-            response: responseData
-        });
+            return new CloudEvent({
+                type: cloudevent.type.replace("Request", "Response"),
+                source: 'source',
+                response: responseData
+            });
+
+        } catch(e: any) {
+            const matches = e.details.match(/Command '(.*)' rejected with code '(.*)': (.*)/);
+            let
+                rejectionType = "UNKNOWN",
+                rejectionReason = e.toString(),
+                ceType = cloudevent.type
+                    .replace("command", "commandRejection")
+                    .replace("ActivateJobsRequest", "JobBatch");
+
+            if (matches) {
+                const intent = matches[1];
+                rejectionType = matches[2];
+                rejectionReason = matches[3];
+                ceType += "." + intent.toLowerCase();
+            }
+
+            return new CloudEvent({
+                type: ceType,
+                source: 'source',
+                response: {
+                    'rejectionType': rejectionType,
+                    'rejectionReason': rejectionReason
+                }
+            });
+        }
     }
 
     async handle(headers: IncomingHttpHeaders, body: string, res: Http2ServerResponse): Promise<void> {
-        const cloudevent = HTTP.toEvent<object>({headers, body});
+        const cloudevent = HTTP.toEvent<any>({headers, body});
 
         let response;
 
-        try {
-            if (Array.isArray(cloudevent))
-                response = await Promise.all(cloudevent.map(async ce => await this._handleCloudevent(ce)));
-            else
-                response = await this._handleCloudevent(cloudevent);
+        if (Array.isArray(cloudevent))
+            response = await Promise.all(cloudevent.map(async ce => await this._handleCloudevent(ce)));
+        else
+            response = await this._handleCloudevent(cloudevent);
 
-            res.writeHead(200);
-            res.write(JSON.stringify(response));
-        } catch(e: any) {
-            res.writeHead(500);
-            console.log(e.code, e.details);
-        } finally {
-            res.end();
-        }
-
+        res.writeHead(200);
+        res.write(JSON.stringify(response));
+        res.end();
     }
 }
